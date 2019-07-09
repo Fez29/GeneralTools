6d4c134b7cadfad0397e78b6f8400bb21c14cb7b
Fez <Fez29@protonmail.com>
2019-07-09 21:27:49 +0200

    fixes applied to script

--- Loki/lokibuildscript.sh
+++ Loki/lokibuildscript.sh
@@ -1,24 +1,53 @@
-bin="lokibuilddir_"
-binpath="$bin$(echo $(date +%m_%d_%Y))"
-GITPATH=https://github.com/jagerman/loki.git
-GITCLONEDIR="loki"
+#testnet
+############## loki Variables ##############
+lokibin="lokibuilddir_"
+Lokibinpath="$lokibin"
+GITLokiPATH=https://github.com/jagerman/loki.git
+GITLokiCLONEDIR="loki"
+LokibinDir="lokibin2"
+LOKI_BRANCH=jdev
+############## Storage Server Variables ##############
+Storagebin="lokistorageserver_"
+Storagebinpath="$StorageServer$(echo $(date +%m_%d_%Y))"
+GITStorageServerPATH=https://github.com/loki-project/loki-storage-server.git
+GITStorageCLONEDIR="loki-storage-server"
+StorageBinDir="lokistorageserverbin"
+STORAGESERVER_BRANCH=dev
 
-n=`ls -d /home/snode/$binpath* | wc -l`
+############## Storage Server ##############
+
+n=`ls -d /home/snode/$Lokibinpath* | wc -l`
 for i in $(echo $n) ; do
 let n=$n+1
 done
 
-DIR="$bin$(echo $(date +%m_%d_%Y))_$(echo 000$n | tail -c 4)"
+#DIR="$lokibin$(echo $(date +%m_%d_%Y))_$(echo 000$n | tail -c 4)"
+DIR="$Lokibinpath$(echo $(date +%m_%d_%Y))_$(echo 000$n | tail -c 4)"
 
-cd && sudo rm -f -r ~/$GITCLONEDIR && git clone --recursive -b jdev $GITPATH && mkdir -p ~/$DIR/logs && cd ~/$DIR && cmake -DENABLE_SYSLOG=ON ~/$GITCLONEDIR && make 1 && cd ~/$DIR/bin/ && \
-~/$DIR/bin/lokid --version > version.txt && ln -snf ~/$DIR/bin/ ~/lokibin
+cd && sudo rm -f -r ~/$GITLokiCLONEDIR && git clone --recursive -b $LOKI_BRANCH $GITLokiPATH && mkdir -p ~/$DIR/logs && cd ~/$DIR && cmake -DENABLE_SYSLOG=ON ~/$GITLokiCLONEDIR && \ 
+make -j 1 && cd ~/$DIR/bin/ && ~/$DIR/bin/lokid --version > version.txt && ln -snf ~/$DIR/bin/ ~/$LokibinDir
+############## End ##############
 
-Version=`cat ~/$DIR/bin/version.txt`
+############## Storage Server ##############
+n=`ls -d /home/snode/$Storagebinpath* | wc -l`
+for i in $(echo $n) ; do
+let n=$n+1
+done
+
+StorageDIR="$Storagebin$(echo $(date +%m_%d_%Y))_$(echo 000$n | tail -c 4)"
+
+cd && sudo rm -f -r ~/$GITStorageCLONEDIR && git clone --recursive -b $STORAGESERVER_BRANCH $GITStorageServerPATH && mkdir -p ~/$StorageDIR/logs && cd ~/$StorageDIR && \ 
+cmake -DENABLE_SYSLOG=ON ~/$GITStorageCLONEDIR && make -j 1 && ln -snf ~/$StorageDIR/bin/ ~/$StorageBinDir
+
+LokiVersion=`cat ~/$DIR/bin/version.txt`
 
 variable=`tput setaf 6`
 text=`tput setaf 2`
 reset=`tput sgr0`
 
-echo "${text}Build files written to ${variable}~/$DIR/bin/${reset} and linked to ${variable}~/lokibin${reset}, use ${variable}~/lokibin/lokid${reset} for example to launch"
+echo "${text}Build files written to ${variable}~/$DIR/bin/${reset} and linked to ${variable}~/$LokibinDir${reset}, use ${variable}~/$LokibinDir/lokid${reset} for example to launch"
 
-echo "${text}Version = ${variable}$Version${reset}"
+echo "${text}Build files written to ${variable}~/$StorageDIR/httpserver/${reset}"
+#and linked to ${variable}~/$LokibinDir${reset}, use ${variable}~/$LokibinDir/lokid${reset} for example to launch"
+
+echo "${text}Version = ${variable}$LokiVersion${reset}"
